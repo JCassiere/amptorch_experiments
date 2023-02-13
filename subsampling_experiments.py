dfcb4d7415ff24206e4e7b11866262cbaf6520d0
JCassiere <jcassiere91@gmail.com>
2023-02-13 17:31:58 -0500

    Added random subsample experiment

--- subsampling_experiments.py
+++ subsampling_experiments.py
@@ -4,6 +4,24 @@ from ccsubsample.subsampling.utils import *
 from ccsubsample.subsampling.evaluation_metrics import *
 from ccsubsample.subsampling.subsampling import faiss_flat_subsample, faiss_ivf_subsample, faiss_ivfpq_subsample, kmeans_subsample
 
+def random_subsampling_experiment(scaled_data, title, num_points_desired):
+    start = time.time()
+    random_subsample = scaled_data[np.random.choice(np.arange(scaled_data.shape[0]), num_points_desired)]
+    wallclock_time = time.time() - start
+    subset_filename = title + "_subset"
+    save_to_numpy(random_subsample, subset_filename, "results")
+    num_points = number_of_points_remaining(random_subsample)
+    _, std_dev = point_diversity_mean_std(random_subsample)
+    experiment_metadata = {
+        "experiment_title": title,
+        "wallclock_time": wallclock_time,
+        "num_points": num_points,
+        "subset_filename": subset_filename,
+        "std_dev": float(std_dev)
+    }
+    metadata_file = open("results/" + title + "_{}_metadata.json".format(time.time()), "w")
+    json.dump(experiment_metadata, metadata_file, indent=4)
+
 
 def kmeans_subsampling_experiment(scaled_data, title, num_points_desired):
     # need - wallclock time, std. dev, number of points, cutoff, and filename for
@@ -59,11 +77,12 @@ def load_qm9_data():
 
 def main():
     scaled_data = load_numpy_data("scaled_and_std_oc20_3k_gmp_fingerprints")
+    random_subsampling_experiment(scaled_data, "oc20_3k_random_subsample", 20000)
     kmeans_subsampling_experiment(scaled_data, "oc20_3k_kmeans_subsample", 20000)
     faiss_subsampling_experiment(scaled_data, faiss_flat_subsample, "oc20_3k_faiss_flat_subsample", cutoff=2)
     faiss_subsampling_experiment(scaled_data, faiss_ivf_subsample, "oc20_3k_faiss_ivf_subsample", cutoff=2)
     faiss_subsampling_experiment(scaled_data, faiss_ivfpq_subsample, "oc20_3k_faiss_ivfpq_subsample", cutoff=2)
-    print(1)
+
 
 if __name__ == "__main__":
     main()
