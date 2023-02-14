c3989f8951f1e500fd3db56e94eb767649b07ecb
JCassiere <jcassiere91@gmail.com>
2023-02-14 16:00:37 -0500

    Added dimensionality reduction experiment

--- data_exchange.py
+++ data_exchange.py
@@ -1,5 +1,7 @@
-import json
 import numpy as np
+import os
+import time
+import json
 
 
 def save_to_numpy(data: np.ndarray, filename: str, prefix: str = "data") -> None:
@@ -10,3 +12,23 @@ def save_to_numpy(data: np.ndarray, filename: str, prefix: str = "data") -> None
 def load_numpy_data(filename: str, prefix: str = "data") -> np.ndarray:
     in_file = open("./{}/{}.npy".format(prefix, filename), "rb")
     return np.load(in_file, allow_pickle=False)
+
+
+def create_dir_if_not_exists(path: str):
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+
+def save_experiment(title: str, subset: np.ndarray, experiment_metadata: json, second_level_dir: str = None):
+    create_dir_if_not_exists("results/")
+    timestamped_title = title + "_{}".format(time.time())
+    if second_level_dir:
+        create_dir_if_not_exists("results/" + second_level_dir)
+        experiment_dir = "results/{}/{}".format(second_level_dir, timestamped_title)
+    else:
+        experiment_dir = "results/{}".format(timestamped_title)
+    create_dir_if_not_exists(experiment_dir)
+    subset_filename = experiment_metadata["subset_filename"]
+    save_to_numpy(subset, subset_filename, experiment_dir)
+    metadata_file = open(experiment_dir + "/metadata.json", 'w')
+    json.dump(experiment_metadata, metadata_file, indent=4)
--- subsampling_experiments.py
+++ subsampling_experiments.py
@@ -1,8 +1,12 @@
 import time
-from data_exchange import save_to_numpy, load_numpy_data
+
+import numpy as np
+
+from data_exchange import save_to_numpy, load_numpy_data, save_experiment
 from ccsubsample.subsampling.utils import *
 from ccsubsample.subsampling.evaluation_metrics import *
 from ccsubsample.subsampling.subsampling import faiss_flat_subsample, faiss_ivf_subsample, faiss_ivfpq_subsample, kmeans_subsample
+
 
 def random_subsampling_experiment(scaled_data, title, num_points_desired):
     start = time.time()
@@ -24,8 +28,6 @@ def random_subsampling_experiment(scaled_data, title, num_points_desired):
 
 
 def kmeans_subsampling_experiment(scaled_data, title, num_points_desired):
-    # need - wallclock time, std. dev, number of points, cutoff, and filename for
-    #  resulting points in an experiment metadata json
     start = time.time()
     indices_to_keep = kmeans_subsample(scaled_data, num_points_desired)
     wallclock_time = time.time() - start
@@ -46,14 +48,11 @@ def kmeans_subsampling_experiment(scaled_data, title, num_points_desired):
 
 
 def faiss_subsampling_experiment(scaled_data, subsample_fn, title, cutoff):
-    # need - wallclock time, std. dev, number of points, cutoff, and filename for
-    #  resulting points in an experiment metadata json
     start = time.time()
     indices_to_keep = subsample_fn(scaled_data, cutoff_sig=cutoff, verbose=2)
     wallclock_time = time.time() - start
     points_to_keep = scaled_data[indices_to_keep]
     subset_filename = title + "_subset"
-    save_to_numpy(points_to_keep, subset_filename, "results")
     num_points = number_of_points_remaining(points_to_keep)
     _, std_dev = point_diversity_mean_std(points_to_keep)
     experiment_metadata = {
@@ -64,19 +63,50 @@ def faiss_subsampling_experiment(scaled_data, subsample_fn, title, cutoff):
         "subset_filename": subset_filename,
         "std_dev": float(std_dev)
     }
-    metadata_file = open("results/" + title + "_{}_metadata.json".format(time.time()), "w")
-    json.dump(experiment_metadata, metadata_file, indent=4)
+    save_experiment(title, points_to_keep, experiment_metadata)
 
-def load_oc20_3k_data():
-    data = pickle.load(open("./data/oc20_train_torch_data.p", "rb"))
-    return data
+# def load_oc20_3k_data():
+#     data = pickle.load(open("./data/oc20_train_torch_data.p", "rb"))
+#     return data
+#
+# def load_qm9_data():
+#     data = pickle.load(open("./data/QM9_train_torch_data.p", "rb"))
+#     return data
 
-def load_qm9_data():
-    data = pickle.load(open("./data/QM9_train_torch_data.p", "rb"))
-    return data
+
+def dimensionality_reduction_experiment(scaled_data: np.ndarray, base_title: str,
+                                        faiss_subsample_method=faiss_ivf_subsample, cutoff: float = 2):
+    pca = PCA(svd_solver="randomized")
+    data_pca = pca.fit_transform(scaled_data)
+    explained_variance_ratio = pca.explained_variance_ratio_
+    _, dim = data_pca.shape
+    
+    second_level_dir = base_title + "_dim_reduction_{}".format(time.time())
+    for i in range(1, dim):
+        start = time.time()
+        indices_to_keep = faiss_subsample_method(np.array(data_pca[:, :i], order='C'), cutoff_sig=cutoff, verbose=2)
+        wallclock_time = time.time() - start
+        points_to_keep = scaled_data[indices_to_keep]
+        title = base_title + "_{}_dimensions".format(i)
+        subset_filename = title + "_subset"
+        num_points = number_of_points_remaining(points_to_keep)
+        _, std_dev = point_diversity_mean_std(points_to_keep)
+        experiment_metadata = {
+            "experiment_title": title,
+            "num_dimensions_kept": i,
+            "variance_retained": np.sum(explained_variance_ratio[:i]),
+            "cutoff": cutoff,
+            "wallclock_time": wallclock_time,
+            "num_points": num_points,
+            "subset_filename": subset_filename,
+            "std_dev": float(std_dev)
+        }
+        save_experiment(title, points_to_keep, experiment_metadata, second_level_dir=second_level_dir)
+
 
 def main():
     scaled_data = load_numpy_data("scaled_and_std_oc20_3k_gmp_fingerprints")
+    dimensionality_reduction_experiment(scaled_data, "oc20_3k_gmp_ivf")
     random_subsampling_experiment(scaled_data, "oc20_3k_random_subsample", 20000)
     kmeans_subsampling_experiment(scaled_data, "oc20_3k_kmeans_subsample", 20000)
     faiss_subsampling_experiment(scaled_data, faiss_flat_subsample, "oc20_3k_faiss_flat_subsample", cutoff=2)
