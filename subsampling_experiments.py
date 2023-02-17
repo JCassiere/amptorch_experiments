dde508a7bcdad7d5ac4d711d9b2f7741d57a076f
JCassiere <jcassiere91@gmail.com>
2023-02-17 18:09:26 -0500

    Added outlier retention to experiments

--- subsampling_experiments.py
+++ subsampling_experiments.py
@@ -10,21 +10,23 @@ from ccsubsample.subsampling.subsampling import faiss_flat_subsample, faiss_ivf_
 
 def random_subsampling_experiment(scaled_data, title, num_points_desired):
     start = time.time()
-    random_subsample = scaled_data[np.random.choice(np.arange(scaled_data.shape[0]), num_points_desired)]
+    indices = np.arange(0, scaled_data.shape[0])
+    random_indices = np.random.choice(np.arange(scaled_data.shape[0]), num_points_desired)
+    random_subsample = scaled_data[random_indices]
     wallclock_time = time.time() - start
     subset_filename = title + "_subset"
-    save_to_numpy(random_subsample, subset_filename, "results")
     num_points = number_of_points_remaining(random_subsample)
     _, std_dev = point_diversity_mean_std(random_subsample)
+    outlier_retention = calculate_outlier_retention(scaled_data, random_indices, outlier_cutoff_modifier=2)
     experiment_metadata = {
         "experiment_title": title,
         "wallclock_time": wallclock_time,
         "num_points": num_points,
         "subset_filename": subset_filename,
-        "std_dev": float(std_dev)
+        "std_dev": float(std_dev),
+        "outlier_retention": outlier_retention
     }
-    metadata_file = open("results/" + title + "_{}_metadata.json".format(time.time()), "w")
-    json.dump(experiment_metadata, metadata_file, indent=4)
+    save_experiment(title, random_subsample, experiment_metadata)
 
 
 def kmeans_subsampling_experiment(scaled_data, title, num_points_desired):
@@ -33,18 +35,18 @@ def kmeans_subsampling_experiment(scaled_data, title, num_points_desired):
     wallclock_time = time.time() - start
     points_to_keep = scaled_data[indices_to_keep]
     subset_filename = title + "_subset"
-    save_to_numpy(points_to_keep, subset_filename, "results")
     num_points = number_of_points_remaining(points_to_keep)
     _, std_dev = point_diversity_mean_std(points_to_keep)
+    outlier_retention = calculate_outlier_retention(scaled_data, indices_to_keep, outlier_cutoff_modifier=2)
     experiment_metadata = {
         "experiment_title": title,
         "wallclock_time": wallclock_time,
         "num_points": num_points,
         "subset_filename": subset_filename,
-        "std_dev": float(std_dev)
+        "std_dev": float(std_dev),
+        "outlier_retention": outlier_retention
     }
-    metadata_file = open("results/" + title + "_{}_metadata.json".format(time.time()), "w")
-    json.dump(experiment_metadata, metadata_file, indent=4)
+    save_experiment(title, points_to_keep, experiment_metadata)
 
 
 def faiss_subsampling_experiment(scaled_data, subsample_fn, title, cutoff):
@@ -55,13 +57,15 @@ def faiss_subsampling_experiment(scaled_data, subsample_fn, title, cutoff):
     subset_filename = title + "_subset"
     num_points = number_of_points_remaining(points_to_keep)
     _, std_dev = point_diversity_mean_std(points_to_keep)
+    outlier_retention = calculate_outlier_retention(scaled_data, indices_to_keep, outlier_cutoff_modifier=2)
     experiment_metadata = {
         "experiment_title": title,
         "cutoff": cutoff,
         "wallclock_time": wallclock_time,
         "num_points": num_points,
         "subset_filename": subset_filename,
-        "std_dev": float(std_dev)
+        "std_dev": float(std_dev),
+        "outlier_retention": outlier_retention
     }
     save_experiment(title, points_to_keep, experiment_metadata)
 
@@ -91,6 +95,7 @@ def dimensionality_reduction_experiment(scaled_data: np.ndarray, base_title: str
         subset_filename = title + "_subset"
         num_points = number_of_points_remaining(points_to_keep)
         _, std_dev = point_diversity_mean_std(points_to_keep)
+        outlier_retention = calculate_outlier_retention(scaled_data, indices_to_keep, outlier_cutoff_modifier=2)
         experiment_metadata = {
             "experiment_title": title,
             "num_dimensions_kept": i,
@@ -99,19 +104,21 @@ def dimensionality_reduction_experiment(scaled_data: np.ndarray, base_title: str
             "wallclock_time": wallclock_time,
             "num_points": num_points,
             "subset_filename": subset_filename,
-            "std_dev": float(std_dev)
+            "std_dev": float(std_dev),
+            "outlier_retention": outlier_retention
         }
         save_experiment(title, points_to_keep, experiment_metadata, second_level_dir=second_level_dir)
 
 
 def main():
     scaled_data = load_numpy_data("scaled_and_std_oc20_3k_gmp_fingerprints")
-    dimensionality_reduction_experiment(scaled_data, "oc20_3k_gmp_ivf")
     random_subsampling_experiment(scaled_data, "oc20_3k_random_subsample", 20000)
     kmeans_subsampling_experiment(scaled_data, "oc20_3k_kmeans_subsample", 20000)
     faiss_subsampling_experiment(scaled_data, faiss_flat_subsample, "oc20_3k_faiss_flat_subsample", cutoff=2)
     faiss_subsampling_experiment(scaled_data, faiss_ivf_subsample, "oc20_3k_faiss_ivf_subsample", cutoff=2)
     faiss_subsampling_experiment(scaled_data, faiss_ivfpq_subsample, "oc20_3k_faiss_ivfpq_subsample", cutoff=2)
+    dimensionality_reduction_experiment(scaled_data, "oc20_3k_gmp_ivf")
+
 
 
 if __name__ == "__main__":
